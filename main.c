8d9bc37e97c57880046c3badc31fccf1c4908147
pawkw <pawkw@user.noreply.github.com>
2022-09-17 23:43:28 -0400

    Added compile result template code.

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include "helpers/vector.h"
+#include "compiler.h"
 
 int main()
 {
-    printf("Hello!\n");
-    return 0;
+    int result = compile_file("./test.c", "test", 0);
+    if(result == COMPILER_FILE_COMPILED_OK)
+        printf("Successfully compiled.\n");
+    else if(result == COMPILER_FAILED_WITH_ERRORS)
+        printf("Compile failed.\n");
+    else
+        printf("Unknown response from compile_file.\n");
+
+    return result;
 }
