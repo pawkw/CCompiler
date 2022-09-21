80b9bfeadaf7d9872ab61b3c0d18bd9e117cbfa1
pawkw <pawkw@user.noreply.github.com>
2022-09-20 21:07:03 -0400

    Created token structures.

--- compiler.h
+++ compiler.h
@@ -2,6 +2,50 @@
 #define PAWC_COMPILER
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    // True if there is whitespace between this and the following token.
+    bool whitespace;
+
+    // A pointer to the first token in the series of tokens within the brackets.
+    // For debugging.
+    const char* between_brackets; 
+
+};
 
 enum {
     COMPILER_FILE_COMPILED_OK,
